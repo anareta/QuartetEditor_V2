a85ea537327215278715e9a9c971abad8c17ecf1
Azoth <vbvbnbnvn@gmail.com>
2016-11-15 23:35:02 +0900

    バージョン情報修正（2.10.2）

--- QuartetEditor/Properties/AssemblyInfo.cs
+++ QuartetEditor/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.10.0.1")]
-[assembly: AssemblyFileVersion("2.10.0.1")]
+[assembly: AssemblyVersion("2.10.0.2")]
+[assembly: AssemblyFileVersion("2.10.0.2")]
 [assembly: NeutralResourcesLanguage("ja")]
 
