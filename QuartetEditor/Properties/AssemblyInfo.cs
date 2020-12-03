f8883bd252e63af650c8c3b3729890b1af6704f2
Az <vbvbnbnvn@gmail.com>
2020-12-03 22:25:58 +0900

    バージョン情報修正（2.13）

--- QuartetEditor/Properties/AssemblyInfo.cs
+++ QuartetEditor/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.12.0.1")]
-[assembly: AssemblyFileVersion("2.12.0.1")]
+[assembly: AssemblyVersion("2.13.0.1")]
+[assembly: AssemblyFileVersion("2.13.0.1")]
 [assembly: NeutralResourcesLanguage("ja")]
 
